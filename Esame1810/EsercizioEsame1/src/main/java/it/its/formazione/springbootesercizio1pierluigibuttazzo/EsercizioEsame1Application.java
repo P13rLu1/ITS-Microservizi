3bdefb8e7cce547bde446f6c35b7e98939ccbeea
P13rLu1 <pbuttazzo@studenti.apuliadigitalmaker.it>
2024-10-18 11:18:23 +0200

    impacchettamento

---
+++ Esame1810/EsercizioEsame1/src/main/java/it/its/formazione/springbootesercizio1pierluigibuttazzo/EsercizioEsame1Application.java
@@ -0,0 +1,13 @@
+package it.its.formazione.springbootesercizio1pierluigibuttazzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioEsame1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsercizioEsame1Application.class, args);
+    }
+
+}
